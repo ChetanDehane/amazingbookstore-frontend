5d13be404c32fad038545b163b2dfcf1c9103dd5
ChetanDehane <chetanpdehane@gmail.com>
2023-12-02 00:44:59 +0530

    added sample testcases

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,12 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("App component", () => {
+
+    it("should render app without error", () => {
+        const { asFragment } = render(<App/>);
+
+        expect(asFragment()).toMatchSnapshot();
+    });
 });
